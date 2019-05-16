1c193e0901a3b20ba24463038d36e486d95b8dd8
Vinh Tran <vincen7.tran@gmail.com>
2019-05-16 16:32:38 -0700

    Axios GET Listings Functional

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = { };
+  }
+
+  componentDidMount() {
+
+  }
+
+  getBookings = () => {
+    axios.get('/listings')
+      .then(data => console.log(data));
   }
 
   render() {
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const Listing = require('../database/listing');
 require('../database/db');
 
 const app = express();
@@ -11,5 +12,14 @@ const distDir = path.join(__dirname, '/../public');
 app.use(express.static(distDir));
 
 app.use(bodyParser.json());
+
+app.get('/listings', async (req, res) => {
+  try {
+    const listings = await Listing.find({});
+    res.send(listings);
+  } catch(e) {
+    res.status(500).send(e);
+  }
+});
 
 app.listen(port, () => console.log('Listening on Port:', port));
