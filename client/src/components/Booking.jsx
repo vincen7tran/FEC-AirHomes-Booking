bc39f6a4559add624e5c419cf584756074a24c00
Vinh Tran <vincen7.tran@gmail.com>
2019-05-17 19:58:10 -0700

    Added GuestForm and CheckingForm Components

--- client/src/components/Booking.jsx
+++ client/src/components/Booking.jsx
@@ -1,63 +1,24 @@
 import React from 'react';
+import CheckingForm from './CheckingForm.jsx';
+import GuestForm from './GuestForm.jsx';
 
 const bookingForm = {
 
 };
 
-const checking = {
-  marginTop: '16px',
-  marginBottom: '8px',
-};
-
-const checkingForms = {
-  display: 'table',
-  tableLayout: 'fixed',
+const buttonDiv = {
+  marginTop: '24px',
   width: '100%',
-  border: '1px solid #ebebeb',
-  borderRadius: '2px',
-};
-
-const checkingRow = {
-  display: 'table-row',
-};
-
-const font12 = {
-  fontSize: '12px',
-  fontWeight: '600',
-  lineHeight: '1.33333em',
-  margin: '0',
-};
-
-const checkingCell = {
-
-};
-
-const formDiv = {
-  fontSize: '17px',
-  lineHeight: '24px',
-  color: '#757575',
   position: 'relative',
   display: 'inline-block',
-  width: '100%',
-  verticalAlign: 'middle',
-  padding: '8px',
+  textAlign: 'center',
+  border: '2px solid #0000',
+  borderRadius: '4px',
 };
 
-const checkForm = {
-  border: 'none',
-};
-
-const arrowContainer = {
-  display: 'table-cell',
-  verticalAlign: 'middle',
-  width: '24px',
-};
-
-const arrow = {
-  height: '24px',
-  width: '24px',
-  display: 'block',
-  fill: 'currentcolor',
+const bookButton = {
+  padding: '10px 22px',
+  background: '#ff5a5f',
 };
 
 class Booking extends React.Component {
@@ -69,28 +30,15 @@ class Booking extends React.Component {
 
   render() {
     return (
-      <div style={bookingForm}>
-        <div style={checking}>
-          <span style={font12}>Date</span>
-          <div style={checkingForms}>
-            <div style={checkingRow}>
-              <div style={checkingCell}>
-                <div style={formDiv}>
-                  <input style={checkForm} type="text" id="checkin" name="checkin" placeholder="Check-in" />
-                </div>
-              </div>
-              <div style={arrowContainer}>
-                <svg viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false" style={arrow}><path d="m0 12.5a.5.5 0 0 0 .5.5h21.79l-6.15 6.15a.5.5 0 1 0 .71.71l7-7v-.01a.5.5 0 0 0 .14-.35.5.5 0 0 0 -.14-.35v-.01l-7-7a .5.5 0 0 0 -.71.71l6.15 6.15h-21.79a.5.5 0 0 0 -.5.5z" fillRule="evenodd" /></svg>
-              </div>
-              <div style={checkingCell}>
-                <div style={formDiv}>
-                  <input style={checkForm} type="text" id="checkout" name="checkout" placeholder="Checkout" />
-                </div>
-              </div>
-            </div>
-          </div>
+      <form action="">
+        <div style={bookingForm}>
+          <CheckingForm />
+          <GuestForm />
         </div>
-      </div>
+        <div style={buttonDiv}>
+          <button type="submit" style={bookButton}></button>
+        </div>
+      </form>
     );
   }
 }
