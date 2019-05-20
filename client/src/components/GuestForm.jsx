acc1323fb20a0e2d8ea9f15ddc288fd31a1bf49f
Vinh Tran <vincen7.tran@gmail.com>
2019-05-19 21:08:04 -0700

    Added SVG to Guest Form

--- client/src/components/GuestForm.jsx
+++ client/src/components/GuestForm.jsx
@@ -28,9 +28,47 @@ const guestButton = {
   borderColor: '#ebebeb',
 };
 
-const buttonDiv = {
+const buttonMargin = {
   marginLeft: '8px',
   marginRight: '8px',
+};
+
+const buttonDiv = {
+  margin: '0px',
+  fontSize: '16px',
+  fontWeight: '400',
+  lineHeight: '1.375em',
+  color: '#484848',
+};
+
+const buttonTable = {
+  display: 'table',
+  width: '100%',
+  borderSpacing: '0px',
+};
+
+const guestCell = {
+  display: 'table-cell',
+  width: '100%',
+  verticalAlign: 'middle',
+};
+
+const guestLabel = {
+  fontSize: '17px',
+  margin: '0',
+  padding: '0',
+};
+
+const svgCell = {
+  display: 'table-cell',
+  verticalAlign: 'middle',
+};
+
+const svgStyle = {
+  height: '16px',
+  width: '16px',
+  display: 'block',
+  fill: 'currencolor',
 };
 
 class GuestForm extends React.Component {
@@ -46,7 +84,20 @@ class GuestForm extends React.Component {
         <span style={font12}>Guests</span>
         <div style={buttonContainer}>
           <button style={guestButton} type="button">
-            <div style={buttonDiv}>1 guest</div>
+            <div style={buttonMargin}>
+              <div style={buttonDiv}>
+                <div style={buttonTable}>
+                  <div style={guestCell}>
+                    <div style={guestLabel}>
+                      <span>1 guest</span>
+                    </div>
+                  </div>
+                  <div style={svgCell}>
+                    <svg viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false" style={svgStyle}><path d="m16.29 4.3a1 1 0 1 1 1.41 1.42l-8 8a1 1 0 0 1 -1.41 0l-8-8a1 1 0 1 1 1.41-1.42l7.29 7.29z" fillRule="evenodd" /></svg>
+                  </div>
+                </div>
+              </div>
+            </div>
           </button>
         </div>
       </div>
