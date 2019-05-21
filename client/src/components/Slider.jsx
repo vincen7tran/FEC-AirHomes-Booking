54979081467ddab01304d370a6878df5abe0b6c2
Vinh Tran <vincen7.tran@gmail.com>
2019-05-21 11:03:37 -0700

    CSS Transition as inline-style

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
+const hide = {
+  maxHeight: '0',
+  opacity: '0',
+  overflow: 'hidden',
+};
+
+const slide = {
+  maxHeight: '500px',
+  opacity: '1',
+  transition: 'opacity 1.5s ease-in, max-height 1s linear',
+};
+
 const Slider = (props) => {
   const { hidden } = props;
   console.log(hidden);
 
   return (
-    <div className={hidden ? 'hide' : 'slide'}>
+    <div style={hidden ? hide : slide}>
       <div>text</div>
       <div>text</div>
       <div>text</div>
