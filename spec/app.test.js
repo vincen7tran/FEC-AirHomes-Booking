2e297a46552159c1bdc6cff9d65dd9167ff45944
Vinh Tran <vincen7.tran@gmail.com>
2019-05-21 16:05:51 -0700

    Rebased Branch and Updated Test

--- spec/app.test.js
+++ spec/app.test.js
@@ -5,7 +5,7 @@ import App from '../client/src/components/App.jsx';
 describe('App', () => {
   it('something', () => {
     const wrapper = shallow(<App />);
-    const hidden = wrapper.state().hidden;
-    expect(hidden).toBe(true);
+    const hideSlideState = wrapper.state().hideSlide;
+    expect(hideSlideState).toBe(true);
   });
 });
