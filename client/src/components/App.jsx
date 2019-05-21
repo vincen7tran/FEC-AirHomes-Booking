948e3aaad036b26515be4866717748e061c4c32c
Vinh Tran <vincen7.tran@gmail.com>
2019-05-20 19:57:14 -0700

    Added Slide Ref

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import Booking from './Booking.jsx';
 import Report from './Report.jsx';
+import Slider from './Slider.jsx';
 
 const body = {
   width: '376px',
@@ -49,9 +50,18 @@ const container = {
   marginLeft: '45px',
 };
 
+const sliderStyle = {
+  postion: 'sticky',
+  top: '-50px',
+  width: '100%',
+  transition: ' top 0.3s',
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.mySlide = React.createRef();
 
     this.state = { listing: {} };
   }
@@ -59,6 +69,18 @@ class App extends React.Component {
   componentDidMount() {
     this.getListing(192)
       .then(({ data }) => this.setState({ listing: data[0] }));
+
+    window.addEventListener('scroll', this.handleScroll());
+  }
+
+  handleScroll = () => {
+    const node = this.mySlide.current;
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      node.style.top = '0';
+    } else {
+      node.style.top = '-50px';
+    }
   }
 
   getListing = listingId => axios.get('/listing', { params: { listingId } });
@@ -82,6 +104,9 @@ class App extends React.Component {
                 </div>
               </div>
               <Booking maxGuests={listing.maxGuests} maxInfants={listing.maxInfants} />
+            </div>
+            <div ref={this.mySlide}>
+              <Slider style={sliderStyle} />
             </div>
           </div>
           <Report />
