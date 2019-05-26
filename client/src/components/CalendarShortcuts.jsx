727ced2a9504bc0425269579cd609cc5a868a6a1
Vinh Tran <vincen7.tran@gmail.com>
2019-05-25 17:50:18 -0700

    CalendarShortcuts Component Complete

--- client/src/components/CalendarShortcuts.jsx
+++ client/src/components/CalendarShortcuts.jsx
@@ -41,6 +41,39 @@ const closeSVG = {
   fill: '#cacccd',
 };
 
+const ulStyle = {
+  fontSize: '14px',
+  listStyle: 'none',
+  padding: '0',
+};
+
+const liStyle = {
+  listStyle: 'none',
+  margin: '6px 0',
+};
+
+const iconDiv = {
+  display: 'inline-block',
+  whiteSpace: 'nowrap',
+  textAlign: 'right',
+  marginRight: '6px',
+};
+
+const iconSpan = {
+  fontFamily: 'monospace',
+  fontSize: '12px',
+  textTransform: 'uppercase',
+  background: '#f2f2f2',
+  padding: '2px 6px',
+};
+
+const liText = {
+  display: 'inline',
+  wordBreak: 'break-word',
+  marginLeft: '8px',
+  fontWeight: '500',
+};
+
 const CalendarShortcuts = (props) => {
   const { onCloseShortcuts } = props;
 
@@ -50,6 +83,50 @@ const CalendarShortcuts = (props) => {
       <button type="button" style={closeButton} onClick={onCloseShortcuts}>
         <svg style={closeSVG} focusable="false" viewBox="0 0 12 12"><path fillRule="evenodd" d="M11.53.47a.75.75 0 0 0-1.061 0l-4.47 4.47L1.529.47A.75.75 0 1 0 .468 1.531l4.47 4.47-4.47 4.47a.75.75 0 1 0 1.061 1.061l4.47-4.47 4.47 4.47a.75.75 0 1 0 1.061-1.061l-4.47-4.47 4.47-4.47a.75.75 0 0 0 0-1.061z" /></svg>
       </button>
+      <ul style={ulStyle}>
+        <li style={liStyle}>
+          <div style={iconDiv}>
+            <span style={iconSpan}>↵</span>
+          </div>
+          <div style={liText}>Select the date in focus</div>
+        </li>
+        <li style={liStyle}>
+          <div style={iconDiv}>
+            <span style={iconSpan}>←/→</span>
+          </div>
+          <div style={liText}>Move backward (left) and forward (right) by one day</div>
+        </li>
+        <li style={liStyle}>
+          <div style={iconDiv}>
+            <span style={iconSpan}>↑/↓</span>
+          </div>
+          <div style={liText}>Move backward (up) and forward (down) by one week</div>
+        </li>
+        <li style={liStyle}>
+          <div style={iconDiv}>
+            <span style={iconSpan}>PGUP/PGDN</span>
+          </div>
+          <div style={liText}>Switch months</div>
+        </li>
+        <li style={liStyle}>
+          <div style={iconDiv}>
+            <span style={iconSpan}>HOME/END</span>
+          </div>
+          <div style={liText}>Go to the first or last day of a week</div>
+        </li>
+        <li style={liStyle}>
+          <div style={iconDiv}>
+            <span style={iconSpan}>ESC</span>
+          </div>
+          <div style={liText}>Return to the date input field</div>
+        </li>
+        <li style={liStyle}>
+          <div style={iconDiv}>
+            <span style={iconSpan}>?</span>
+          </div>
+          <div style={liText}>Open this panel</div>
+        </li>
+      </ul>
     </div>
   );
 };
